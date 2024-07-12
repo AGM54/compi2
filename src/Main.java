da9745584b798d45d47ee829da050111f4f13ddc
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-07-12 01:05:43 -0600

    Manejo de errores
    manejo de errores

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,36 @@ public class Main {
         try {
             CharStream input = CharStreams.fromFileName(inputFile);
             MiniLangLexer lexer = new MiniLangLexer(input);
+
+            // Manejo de errores en el lexer
+            lexer.removeErrorListeners();
+            lexer.addErrorListener(new BaseErrorListener() {
+                @Override
+                public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                    System.err.println("Lexer error at line " + line + ":" + charPositionInLine + " - " + msg);
+                }
+            });
+
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             MiniLangParser parser = new MiniLangParser(tokens);
+
+            // Manejo de errores en el parser
+            parser.removeErrorListeners();
+            parser.addErrorListener(new BaseErrorListener() {
+                @Override
+                public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                    System.err.println("Parser error at line " + line + ":" + charPositionInLine + " - " + msg);
+                }
+            });
+
             ParseTree tree = parser.prog();
 
-            EvalVisitor eval = new EvalVisitor();
-            eval.visit(tree);
+            if (parser.getNumberOfSyntaxErrors() == 0) {
+                EvalVisitor eval = new EvalVisitor();
+                eval.visit(tree);
+            } else {
+                System.err.println("Parsing failed due to syntax errors.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
